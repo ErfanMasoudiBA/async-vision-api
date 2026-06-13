082d021cbbdd1fcbe156f0ea2932c5c0fd339dfe
ErfanmasoudiBA <ErfanmasoudiBA@gmail.com>
2026-06-13 21:42:44 +0330

    feat: add well-response actions for Failure/ Pending/ Successful celery's job.

--- app/api/v1/celery_worker.py
+++ app/api/v1/celery_worker.py
@@ -1,7 +1,8 @@
-from app.celery_app import app as celery_app
-from app.services.tasks import add
 from celery.result import AsyncResult
 from fastapi import APIRouter
+
+from app.celery_app import app as celery_app
+from app.services.tasks import add
 
 router = APIRouter()
 
@@ -9,13 +10,28 @@ router = APIRouter()
 @router.post("/run-add-task")
 def run_add_task(x: int, y: int):
     result = add.delay(x, y)
+    print(result.id)
+    print(result.status)
+    print(result.state)
+    print(result.ready())
+    print(result.successful())
     return {"message": "Task queued successfully", "celery_task_id": result.id}
 
 
 @router.get("/status/{celery_task_id}")
 def get_add_result(celery_task_id: str):
     result = AsyncResult(celery_task_id, app=celery_app)
-    print(result)
-    if result.ready():
+    # print(result)
+    # print(result.id)
+    # print(result.status)
+    # print(result.state)
+    # print(result.ready())
+    # print(result.successful())
+    # print(result.result)
+    if result.state == "PENDING":
+        return {"message": "pending"}
+    if result.state == "FAILURE":
+        return {"status": "failed", "error": str(result.info)}
+    if result.successful():
         return {"status": "completed", "result": result.result}
-    return {"status": "processing"}
+    return {"status": result.state.lower()}
