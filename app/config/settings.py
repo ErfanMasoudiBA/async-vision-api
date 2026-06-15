c7d24c7356a5039b2ba2f4f3879439c4b5233171
ErfanmasoudiBA <ErfanmasoudiBA@gmail.com>
2026-06-15 11:47:21 +0330

    feat: add dockerization setup for api, worker and redis

--- app/config/settings.py
+++ app/config/settings.py
@@ -22,7 +22,9 @@ class Settings(BaseSettings):
     CELERY_BROKER_URL: str
     CELERY_RESULT_BACKEND: str
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
+    )
 
 
 @lru_cache
